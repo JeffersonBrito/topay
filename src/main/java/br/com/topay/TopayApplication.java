1b63299d39e5ff32b87a8f7af9d84601a53e6646
Jefferson Brito <jefersonbritoalm@gmail.com>
2020-05-12 23:20:31 -0300

    Project created

---
+++ src/main/java/br/com/topay/TopayApplication.java
@@ -0,0 +1,13 @@
+package br.com.topay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/topay/TopayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.topay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
